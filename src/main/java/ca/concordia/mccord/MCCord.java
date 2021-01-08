7d819fb2f71abde668fce4c51e7b36db06c702dc
alexandre-lavoie <alexandre.lavoie00@gmail.com>
2021-01-08 15:53:45 -0500

    Template forge mod structure.

---
+++ src/main/java/ca/concordia/mccord/MCCord.java
@@ -0,0 +1,10 @@
+package ca.concordia.mccord;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Resources.MOD_ID)
+public class MCCord {
+    public MCCord() {
+        Resources.LOGGER.debug("MCCord Loaded");
+    }
+}
---
+++ src/main/java/ca/concordia/mccord/Resources.java
@@ -0,0 +1,9 @@
+package ca.concordia.mccord;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Resources {
+    public static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "mccord";
+}
